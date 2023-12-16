b3324fb9389ee6f5859297525a942649d55bda4a
Yash <yash_911925@student.nitw.ac.in>
2023-12-16 10:27:05 +0530

    ll2

---
+++ yash/linkedlist/linkedlist.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class llnode{
+
+	public:
+		int value;
+		llnode* next;
+
+		llnode(int n){
+			this-> value = n;
+			this-> next = NULL;
+		}
+
+};
+
+		void insertathead(llnode* &head,int data){
+		llnode* temp2 = new llnode(data);
+		temp2->next = head;
+		head = temp2;
+
+		}
+		void print(llnode* head){
+		while(head!=NULL){cout<<head->value<<" ";head=head->next;}
+cout<<endl;
+		}
+
+void insertattail(llnode* &tail,int data){
+
+	llnode* temp = new llnode(data);
+	tail-> next = temp;
+	tail = temp;
+
+
+
+}
+
+
+void insertinmiddle(llnode* &head, int position, int data){
+	llnode* temp = new llnode(data); 
+	llnode* temp2 = head;
+	int count = 1;
+	while(count<position){
+		temp2 = temp2->next;
+		count++;
+	}
+	temp->next = temp2->next;
+	temp2->next = temp;
+
+}
+
+
+
+int main(){
+llnode* temp = new llnode(5);
+llnode* head = temp;
+llnode* tail = temp;
+insertathead(head,1);
+insertattail(tail,7);
+insertathead(head,2);
+insertinmiddle(head,3,12);
+insertattail(tail,10);
+print(head);
+}
