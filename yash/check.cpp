07932684287a11a081ca9a6275ddce98e66dc59a
Yash <yash_911925@student.nitw.ac.in>
2023-12-17 18:15:38 +0530

    comit

---
+++ yash/check.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+char ch = 'P';
+
+switch (ch){
+
+int c = 5+4;
+cout<<c;
+
+	case 'P': cout<<ch; break;case 'E': cout<<'A'; break;
+
+
+
+
+
+}
+
+
+}
